4d145c78b3f1743a5d1c1f9fec3e753358de2695
goyal-rachit <rachit@captur.ai>
2024-05-10 10:09:27 +0530

    adding captur object class

---
+++ capturMicroMobilityAndroidUiLib/src/main/java/com/captur/capturmicromobilityandroiduilib/Captur.kt
@@ -0,0 +1,14 @@
+package com.captur.capturmicromobilityandroiduilib
+
+import android.content.Context
+import android.widget.Toast
+
+/**
+created by Rachit on 5/10/2024.
+ */
+object Captur {
+
+    fun showToast(context: Context){
+      Toast.makeText(context,"Message",Toast.LENGTH_SHORT).show()
+    }
+}
