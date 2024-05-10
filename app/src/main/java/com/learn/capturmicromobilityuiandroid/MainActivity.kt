163f9146bd93cd3146f102326dc2b1dea617f4eb
goyal-rachit <rachit@captur.ai>
2024-05-10 09:51:55 +0530

    testing

--- app/src/main/java/com/learn/capturmicromobilityuiandroid/MainActivity.kt
+++ app/src/main/java/com/learn/capturmicromobilityuiandroid/MainActivity.kt
@@ -17,9 +17,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             CapturMicroMobilityUiAndroidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(modifier = Modifier.fillMaxSize())
+                { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
